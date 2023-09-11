4c261e426fb6efb673d2a48caf93af116ce61c06
Dangkeys <TajdangZaa@gmail.com>
2023-09-11 11:37:17 +0700

    Added win lose condition and LoadScene

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player"))
+            Invoke("ReloadScene", loadDelay);
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
