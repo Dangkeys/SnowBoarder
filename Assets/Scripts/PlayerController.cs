e641158efd9d4b5f2a816ed0189a9b0b9cefca9d
Dangkeys <TajdangZaa@gmail.com>
2023-09-12 21:04:09 +0700

    Added Particle system and Fixed PlayerMovement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 10;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    SurfaceEffector2D surfaceEffector2D;
     Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
-            rb2d.AddTorque(torqueAmount);
-        else if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
-            rb2d.AddTorque(-torqueAmount);
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+            surfaceEffector2D.speed = boostSpeed;
+        else
+            surfaceEffector2D.speed = baseSpeed;
+    }
+
+    private void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            rb2d.AddTorque(torqueAmount * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            rb2d.AddTorque(-torqueAmount * Time.deltaTime);
     }
 }
